adc2245a9e43b5b942c8f64fb2ad9bebfed330c8
Lorenz Gaertner <lorenz.gaertner@gmail.com>
2023-12-04 17:02:50 +0100

    knunu setup for combination

--- examples/knunu_sample.py
+++ examples/knunu_sample.py
@@ -16,8 +16,10 @@ unconstr_priors = {
     'mu':  {'type': 'Normal_Unconstrained',  'mu':    [1.], 'sigma': [1e-10]},
     'cvl': {'type': 'Uniform_Unconstrained', 'lower': [3.], 'upper': [7.]},
     'cvr': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]},
+    # 'cvr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [3.]},
     'csl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [3.]},
     'csr': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]},
+    # 'csr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [3.]},
     'ctl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [3.]}
 }
 
@@ -26,7 +28,7 @@ priorDict_conjugate
 
 n_draws = 10000
 with infer.model(model, unconstr_priors, alt_yields):
-    post_data = pm.sample(draws=n_draws, tune=1500)
+    post_data = pm.sample(draws=n_draws, tune=2000)
     post_pred = pm.sample_posterior_predictive(post_data)
     prior_pred = pm.sample_prior_predictive(n_draws)
 
