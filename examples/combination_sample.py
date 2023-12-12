093906c2bf99054277acad105ee2dafffdfb954a
Lorenz Gaertner <lorenz.gaertner@physik.uni-muenchen.de>
2023-12-12 14:58:04 +0100

    first tries with normal distribution

--- examples/combination_sample.py
+++ examples/combination_sample.py
@@ -11,17 +11,17 @@ files       = ['knunu_model.json', 'ksnunu_model.json']
 alt_dists   = [knunu_utils.alt_pred().distribution, ksnunu_utils.alt_pred().distribution]
 null_dists  = [knunu_utils.null_pred().distribution, ksnunu_utils.null_pred().distribution]
 
-model, data = modifier.combine(files, alt_dists, null_dists, return_data=True)
+model, data = modifier.combine(files, alt_dists, null_dists, return_data=True, clip_bin_data=5.)
 yields = data[:model.config.nmaindata]
 
 # Perform the sampling
 unconstr_priors = {
     'mu':  {'type': 'Normal_Unconstrained',  'mu':    [1.], 'sigma': [1e-10]},
-    'cvl': {'type': 'Uniform_Unconstrained', 'lower': [3.], 'upper': [7.]},
-    'cvr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [2.]},
-    'csl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [2.]},
-    'csr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [2.]},
-    'ctl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [2.]}
+    'cvl': {'type': 'Normal_Unconstrained',  'mu':    [6.], 'sigma': [5.]},
+    'cvr': {'type': 'HalfNormal_Unconstrained',             'sigma': [5.]},
+    'csl': {'type': 'HalfNormal_Unconstrained',             'sigma': [5.]},
+    'csr': {'type': 'HalfNormal_Unconstrained',             'sigma': [5.]},
+    'ctl': {'type': 'HalfNormal_Unconstrained',             'sigma': [5.]}
 }
 
 priorDict_conjugate = prepare_inference.build_priorDict(model, unconstr_priors)
@@ -33,6 +33,6 @@ with infer.model(model, unconstr_priors, yields):
     post_pred = pm.sample_posterior_predictive(post_data)
     prior_pred = pm.sample_prior_predictive(n_draws)
 
-post_data.to_json( 'samples/comb_post_data.json')
-post_pred.to_json( 'samples/comb_post_pred.json')
-prior_pred.to_json('samples/comb_prior_pred.json')
+post_data.to_json( 'samples/comb_normal_post_data.json')
+post_pred.to_json( 'samples/comb_normal_post_pred.json')
+prior_pred.to_json('samples/comb_normal_prior_pred.json')
