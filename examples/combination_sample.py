b2519c10a4ac6a20346a2db5240e6bde6866b074
Lorenz Gaertner <lorenz.gaertner@gmail.com>
2023-12-14 13:51:24 +0100

    specify 1b sampling

--- examples/combination_sample.py
+++ examples/combination_sample.py
@@ -3,8 +3,8 @@ from redist import modifier
 import knunu_utils 
 import ksnunu_utils 
 
-from Bayesian_pyhf import infer
-from Bayesian_pyhf import prepare_inference
+from bayesian_pyhf import infer
+from bayesian_pyhf import prepare_inference
 import pymc as pm
 
 files       = ['knunu_model_1b.json', 'ksnunu_model_1b.json']
@@ -17,19 +17,19 @@ yields = data[:model.config.nmaindata]
 # Perform the sampling
 unconstr_priors = {
     'mu':  {'type': 'Normal_Unconstrained',  'mu':    [1.], 'sigma': [1e-10]},
-    'cvl': {'type': 'Uniform_Unconstrained', 'lower': [3.], 'upper': [7.]},
-    'cvr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [2.]},
-    'csl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [2.]},
-    'csr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [2.]},
-    'ctl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [2.]}
+    'cvl': {'type': 'Uniform_Unconstrained', 'lower': [3.], 'upper': [9.]},
+    'cvr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [6.]},
+    'csl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [6.]},
+    'csr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [6.]},
+    'ctl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [6.]}
 }
 
 priorDict_conjugate = prepare_inference.build_priorDict(model, unconstr_priors)
 priorDict_conjugate
 
-n_draws = 10000
+n_draws = 100000
 with infer.model(model, unconstr_priors, yields):
-    post_data = pm.sample(draws=n_draws, tune=2000)
+    post_data = pm.sample(draws=n_draws, tune=10000)
     post_pred = pm.sample_posterior_predictive(post_data)
     prior_pred = pm.sample_prior_predictive(n_draws)
 
