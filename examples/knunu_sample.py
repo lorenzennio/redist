c1b510a0322cebea0a9a7d8ce6382b59f8cb57a6
Lorenz Gaertner <lorenz.gaertner@gmail.com>
2023-12-14 11:26:28 +0100

    knunu normal constraint

--- examples/knunu_sample.py
+++ examples/knunu_sample.py
@@ -2,25 +2,23 @@ import numpy as np
 from redist import modifier
 import knunu_utils
 
-from Bayesian_pyhf import infer
-from Bayesian_pyhf import prepare_inference
+from bayesian_pyhf import infer
+from bayesian_pyhf import prepare_inference
 import pymc as pm
 
 null = knunu_utils.null_pred()
 alt = knunu_utils.alt_pred()
 
-model, alt_yields = modifier.load('knunu_model.json', alt.distribution, null.distribution, return_data=True)
+model, alt_yields = modifier.load('knunu_model.json', alt.distribution, null.distribution, return_data=True, clip_bin_data=5.)
 
 # Perform the sampling
 unconstr_priors = {
     'mu':  {'type': 'Normal_Unconstrained',  'mu':    [1.], 'sigma': [1e-10]},
-    'cvl': {'type': 'Uniform_Unconstrained', 'lower': [3.], 'upper': [7.]},
+    'cvl': {'type': 'Normal_Unconstrained',  'mu':    [6.6], 'sigma': [10]},
     'cvr': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]},
-    # 'cvr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [3.]},
-    'csl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [3.]},
+    'csl': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [10]},
     'csr': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]},
-    # 'csr': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [3.]},
-    'ctl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [3.]}
+    'ctl': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [10]},
 }
 
 priorDict_conjugate = prepare_inference.build_priorDict(model, unconstr_priors)
