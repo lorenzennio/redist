e4867b447dc8f53210854420682fe90a7facf7c7
Lorenz Gaertner <lorenz.gaertner@gmail.com>
2023-12-22 15:55:27 +0100

    knunu adapt samples to new model

--- examples/knunu_sample.py
+++ examples/knunu_sample.py
@@ -9,16 +9,16 @@ import pymc as pm
 null = knunu_utils.null_pred()
 alt = knunu_utils.alt_pred()
 
-model, alt_yields = modifier.load('knunu_model.json', alt.distribution, null.distribution, return_data=True, clip_bin_data=0.1)
+model, alt_yields = modifier.load('knunu_model.json', alt.distribution, null.distribution, return_data=True, clip_bin_data=1e-6)
 
 # Perform the sampling
 unconstr_priors = {
     'mu':  {'type': 'Normal_Unconstrained',  'mu':    [1.], 'sigma': [1e-10]},
-    'cvl': {'type': 'Uniform_Unconstrained', 'lower': [3.], 'upper': [9.]},
+    'cvl': {'type': 'Uniform_Unconstrained', 'lower': [5.], 'upper': [20.]},
     'cvr': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]},
-    'csl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [6.]},
+    'csl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [15.]},
     'csr': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]},
-    'ctl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [6.]}
+    'ctl': {'type': 'Uniform_Unconstrained', 'lower': [0.], 'upper': [15.]}
 }
 
 priorDict_conjugate = prepare_inference.build_priorDict(model, unconstr_priors)
