045a8336733bb62e4f26a3fd5823ce864d2e8845
Lorenz Gaertner <lorenz.gaertner@gmail.com>
2023-12-24 15:35:59 +0100

    update pilnu

--- examples/pilnu/pilnu_sample.py
+++ examples/pilnu/pilnu_sample.py
@@ -2,31 +2,33 @@ import numpy as np
 from redist import modifier
 import pilnu_utils
 
-from Bayesian_pyhf import infer
-from Bayesian_pyhf import prepare_inference
+from bayesian_pyhf import infer
+from bayesian_pyhf import prepare_inference
 import pymc as pm
 
 null = pilnu_utils.null_pred()
 alt = pilnu_utils.alt_pred()
 
-model, alt_yields = modifier.load('pilnu_model.json', alt.distribution, null.distribution, return_data=True)
+model, alt_yields = modifier.load('pilnu_model.json', alt.distribution, null.distribution, return_data=True, clip_bin_data=0.1)
 
 # Perform the sampling
 unconstr_priors = {
-    'mu':  {'type': 'Normal_Unconstrained',  'mu':    [ 1.0], 'sigma': [1e-10]},
-    'cvl': {'type': 'Uniform_Unconstrained', 'lower': [-0.5], 'upper': [2.]},
-    'csl': {'type': 'Uniform_Unconstrained', 'lower': [-0.5], 'upper': [2.]},
-    'ct' : {'type': 'Uniform_Unconstrained', 'lower': [-0.5], 'upper': [2.]}
+    'mu':  {'type': 'Normal_Unconstrained',  'mu':    [1.0], 'sigma': [1e-10]},
+    'cvl': {'type': 'Uniform_Unconstrained', 'lower': [0.0], 'upper': [2.]},
+    'cvr': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]},
+    'csl': {'type': 'Uniform_Unconstrained', 'lower': [0.0], 'upper': [2.]},
+    'csr': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]},
+    'ctl': {'type': 'Normal_Unconstrained',  'mu':    [0.], 'sigma': [1e-10]}
 }
 
 priorDict_conjugate = prepare_inference.build_priorDict(model, unconstr_priors)
 
-n_draws = 10000
+n_draws = 1000
 with infer.model(model, unconstr_priors, alt_yields):
-    post_data = pm.sample(draws=n_draws, tune=1500)
+    post_data = pm.sample(draws=n_draws, tune=100)
     post_pred = pm.sample_posterior_predictive(post_data)
     prior_pred = pm.sample_prior_predictive(n_draws)
 
-post_data.to_json( 'samples/pilnu_post_data.json')
-post_pred.to_json( 'samples/pilnu_post_pred.json')
-prior_pred.to_json('samples/pilnu_prior_pred.json')
+post_data.to_json( '../samples/pilnu_post_data.json')
+post_pred.to_json( '../samples/pilnu_post_pred.json')
+prior_pred.to_json('../samples/pilnu_prior_pred.json')
--- examples/pilnu/pilnu_utils.py
+++ examples/pilnu/pilnu_utils.py
@@ -11,16 +11,18 @@ def analysis():
     """
 
     analysis_args = {
-        'priors': [
-            { 'parameter': 'B->D::alpha^f+_0@BSZ2015', 'min':  0.0, 'max':  1.0, 'type': 'uniform' },
-            { 'parameter': 'B->D::alpha^f+_1@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
-            { 'parameter': 'B->D::alpha^f+_2@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
-            { 'parameter': 'B->D::alpha^f0_1@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
-            { 'parameter': 'B->D::alpha^f0_2@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' }
+        'priors': [      
+            { 'parameter': 'B->pi::alpha^f+_0@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
+            { 'parameter': 'B->pi::alpha^f+_1@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
+            { 'parameter': 'B->pi::alpha^f+_2@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
+            { 'parameter': 'B->pi::alpha^f0_1@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
+            { 'parameter': 'B->pi::alpha^f0_2@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
+            { 'parameter': 'B->pi::alpha^fT_0@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
+            { 'parameter': 'B->pi::alpha^fT_1@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
+            { 'parameter': 'B->pi::alpha^fT_2@BSZ2015', 'min': -5.0, 'max': +5.0, 'type': 'uniform' },
         ],
         'likelihood': [
-            'B->D::f_++f_0@HPQCD:2015A',
-            'B->D::f_++f_0@FNAL+MILC:2015B'
+            'B->pi::FormFactors[parametric,LCSRLattice]@GKvD:2018A'
         ]
     }
 
@@ -40,7 +42,8 @@ class null_pred:
     Null (SM) prediction
     """
     def __init__(self):
-        p = eos.Parameters()
+        self.ana = analysis()
+        p = self.ana.parameters
         o = eos.Options({'form-factors': 'BSZ2015', 'l': 'tau', 'model':'WET'})
         k = eos.Kinematics({'q2': 5.0, 'cos(theta_l)': 0.0,})
 
@@ -81,27 +84,37 @@ class alt_pred:
         
         self.kv1 = k['q2']
         self.kv2 = k['cos(theta_l)']
-        self.wc1 = p['ubtaunutau::Re{cVL}'     ]
-        self.wc2 = p['ubtaunutau::Re{cSL}'     ]
-        self.wc3 = p['ubtaunutau::Re{cT}'      ]
-        self.hv1 = p['B->D::alpha^f+_0@BSZ2015']
-        self.hv2 = p['B->D::alpha^f+_1@BSZ2015']
-        self.hv3 = p['B->D::alpha^f+_2@BSZ2015']
-        self.hv4 = p['B->D::alpha^f0_1@BSZ2015']
-        self.hv5 = p['B->D::alpha^f0_2@BSZ2015']
+        self.wc1 = p['ubtaunutau::Re{cVL}']
+        self.wc2 = p['ubtaunutau::Re{cVR}']
+        self.wc3 = p['ubtaunutau::Re{cSL}']
+        self.wc4 = p['ubtaunutau::Re{cSR}']
+        self.wc5 = p['ubtaunutau::Re{cT}' ]
+        self.hv1 = p['B->pi::alpha^f+_0@BSZ2015']
+        self.hv2 = p['B->pi::alpha^f+_1@BSZ2015']
+        self.hv3 = p['B->pi::alpha^f+_2@BSZ2015']
+        self.hv4 = p['B->pi::alpha^f0_1@BSZ2015']
+        self.hv5 = p['B->pi::alpha^f0_2@BSZ2015']
+        self.hv6 = p['B->pi::alpha^fT_0@BSZ2015']
+        self.hv7 = p['B->pi::alpha^fT_1@BSZ2015']
+        self.hv8 = p['B->pi::alpha^fT_2@BSZ2015']
         
         self.obs = eos.Observable.make('B->pilnu::d^2BR/dq2/dcos(theta_l)', p, k, o)
 
 
-    def distribution(self, q2, costl, cvl, csl, ct, fp0, fp1, fp2, f01, f02):
+    def distribution(self, q2, costl, cvl, cvr, csl, csr, ctl, fp0, fp1, fp2, f01, f02, ft0, ft1, ft2):
         self.wc1.set(cvl)
-        self.wc2.set(csl)
-        self.wc3.set(ct )
+        self.wc2.set(cvr)
+        self.wc3.set(csl)
+        self.wc4.set(csr)
+        self.wc5.set(ctl)
         self.hv1.set(fp0)
         self.hv2.set(fp1)
         self.hv3.set(fp2)
         self.hv4.set(f01)
         self.hv5.set(f02)
+        self.hv6.set(ft0)
+        self.hv7.set(ft1)
+        self.hv8.set(ft2)
 
         if isinstance(q2, numbers.Number) and isinstance(costl, numbers.Number):
             self.kv1.set(q2)
