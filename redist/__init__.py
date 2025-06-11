3fc14e404d3935d74af9e298be3eaa4dd88cb99e
Lorenz Gaertner <lorenz.gaertner@physik.uni-muenchen.de>
2025-06-11 16:42:58 +0200

    add correct versioning

--- redist/__init__.py
+++ redist/__init__.py
@@ -2,4 +2,7 @@ import redist.modifier as modifier
 import redist.plot as plot
 import redist.custom_modifier as custom_modifier
 
-__all__ = ["modifier", "plot", "custom_modifier"]
+# Convenient access to the version number
+from ._version import version as __version__
+
+__all__ = ["modifier", "plot", "custom_modifier", "__version__"]
