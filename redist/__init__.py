1a7d386c197505decb54a5436d33eeb89a06306a
Marco Colonna <mcolonna@rhino.e5.physik.tu-dortmund.de>
2025-06-13 17:11:26 +0200

    First Commit: Hammer backup

--- redist/__init__.py
+++ redist/__init__.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import redist.modifier as modifier
 import redist.plot as plot
 import redist.custom_modifier as custom_modifier
-=======
-import redist.modifier
-import redist.plot
-import redist.custom_modifier
->>>>>>> d548d0023030b09b3854902dfad7e6b02d4dbf27
 
 __all__ = ["modifier", "plot", "custom_modifier"]
